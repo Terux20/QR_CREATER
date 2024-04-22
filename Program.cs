8f32dda1b10b240a78c389705ff47e24ede5a2d8
Terux20 <155570356+Terux20@users.noreply.github.com>
2024-04-22 11:31:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using QRCoder;
+namespace QR_KOD_OLUŞTURUCU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("QR kodunu oluşturmak istediğiniz metni girin girin");
+            string number = Console.ReadLine();
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(number.ToString(), QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            qrCodeImage.Save("qrcode.png", System.Drawing.Imaging.ImageFormat.Png);
+            Console.WriteLine($"Sayı: {number} QR kod olarak oluşturuldu ve 'qrcode.png' olarak kaydedildi.");
+        }
+    }
+}
